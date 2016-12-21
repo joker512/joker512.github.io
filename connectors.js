92c2650dccab18e2adbb96c69bec3651f1cf337a
Sergey Kotsur <kotsursv@gmail.com>
2016-12-21 12:48:06 +0300

    code refactoring

--- connectors.js
+++ connectors.js
@@ -1,35 +1,18 @@
-var chart_config = {
-	chart: {
-		container: "#taskTreeChart",
-		levelSeparation: 20,
-		rootOrientation: "WEST",
-		connectors: {
-			type: "curve",
-			style: {
-				"stroke-width": 2,
-				stroke: "#50688D"
-			}
-		},
-		node: {
-			HTMLclass: "cell"
-		}
-	},
-	nodeStructure: {
-		text: { name: "" },
-		pseudo: "true",
-		children: JSON.parse(decodeURIComponent(location.search.split('tree=')[1]))
-	}
-};
+var MAX_ELEMENT_LEN = 12;
+var MAX_PAIR_LEN = 10;
+var MAX_LEAVES = 6;
+var DEFAULT_PRIORITY = 1;
 
-function genImage(name, image, visibility, action) {
-	return "<img class=\"pictogram\" id=\"" + name + action + "\" style=\"display:" + (visibility ? "block" : "none") + "\" src=\"" + image + "\" onclick=" + action + "(\"" + name + "\")>";
+function btnImageHTML(name, image, visibility, action) {
+	return "<img =\"" + name + action + "\" style=\"display:" + (visibility ? "block" : "none")
+		+ ";padding:2px\" src=\"" + image + "\" onclick=" + action + "(\"" + name + "\")>";
 }
 
-function genPrioritySelect(node) {
+function prioritySelectHTML(node) {
 	var priorityHtml = "";
 	if (node.text.priority != null) {
-		priorityHtml = "<select name=\"" + node.text.name + "\" onchange=changePriority(this)>";
-		for(var i = 1; i <= 6; i++) {
+		priorityHtml = "<select name=\"" + node.text.name + "\" onchange=priorityChanged(this)>";
+		for(var i = 1; i <= MAX_LEAVES; i++) {
 			if (i != node.text.priority)
 				priorityHtml += "<option>" + i + "</option>";
 			else
@@ -40,15 +23,27 @@ function genPrioritySelect(node) {
 	return priorityHtml;
 }
 
-function innerHTML(node, child) {
-	var haveCreate = node.children[node.children.length - 1].text ? 0 : 1;
-	var lastIndex = node.children.length - 1 - haveCreate;
-	return "<input type=input value=" + child.text.value + " name=\"" + child.text.name + "\" maxlength=" + (child.children ? 8 : 12) + " size=10 onchange=changeText(this)></input><p>"
-		+ genImage(child.text.name, "remove2.png", !node.text.name && !(!node.children[0].children && !node.children[1].children && !node.children[2].text), "removeNode")
-		+ genImage(child.text.name, "down.png", node.children.indexOf(child) < lastIndex, "down")
-		+ genImage(child.text.name, "up.png", node.children.indexOf(child) > 0, "up")
-		+ genImage(child.text.name, "union.png", !node.text.name && node.children.length > 2 + haveCreate && node.children.indexOf(child) < lastIndex, "union")
-		+ genPrioritySelect(child)
+function nodeHTML(root, child) {
+	var rName = root.text.name;
+	var cName = child.text.name;
+	var rChildren = root.children;
+	var create = rChildren[rChildren.length - 1].text ? 0 : 1;
+	var lastIndex = rChildren.length - 1 - create;
+
+	var removeVisible = !rName && (child.children || (rChildren[2] && rChildren[2].text));
+	var downVisible = rChildren.indexOf(child) < lastIndex;
+	var upVisible = rChildren.indexOf(child) > 0;
+	var unionVisible = !rName && rChildren.length > 2 + create && rChildren.indexOf(child) < lastIndex;
+
+	return "<input type=input value=" + child.text.value
+		+ " name=\"" + cName + "\" maxlength=" + (child.children ? MAX_PAIR_LEN : MAX_ELEMENT_LEN)
+		+ " size=10 onchange=textChanged(this)></input>"
+		+ "<p>"
+		+ btnImageHTML(cName, "remove.png", removeVisible, "removeClick")
+		+ btnImageHTML(cName, "down.png", downVisible, "downClick")
+		+ btnImageHTML(cName, "up.png", upVisible, "upClick")
+		+ btnImageHTML(cName, "union.png", unionVisible, "unionClick")
+		+ prioritySelectHTML(child)
 		+ "</p>"
 }
 
@@ -56,79 +51,92 @@ function init(node) {
 	if (node.children) {
 		for(var i = 0; i < node.children.length; i++) {
 			var child = node.children[i];
-			if (!child.text)
-				continue;
-			if (!child.text.value)
-				child.text.value = child.text.name;
-			child.innerHTML = innerHTML(node, child);
-			child.parent = node;
-			init(child);
+			if (!child.text) {
+				hasInsertNode = true;
+			}
+			else {
+				if (!child.text.value)
+					child.text.value = child.text.name;
+				child.innerHTML = nodeHTML(node, child);
+				child.parent = node;
+				init(child);
+			}
 		}
 	}
 	else {
 		leavesCount++;
 	}
+
+	if (node.text.name == "" && !hasInsertNode && leavesCount < MAX_LEAVES) {
+		var createNode = {
+			innerHTML: "<img src=\"create.png\" onclick=createClick()>",
+			HTMLclass: "insert"
+		};
+		node.children.splice(node.children.length, 0, createNode);
+	}
 }
 
-function clear(node) {
+function clean(node) {
 	if (node.children) {
-		for(var i = 0; i < node.children.length; i++) {
+		var len = node.children.length;
+		for(var i = 0; i < len; i++) {
 			var child = node.children[i];
 			delete child['innerHTML'];
 			delete child['parent'];
-			clear(child);
+			clean(child);
+		}
+		if (node.children[len - 1]['HTMLclass']) {
+			node.children.splice(len - 1, 1);
 		}
 	}
 }
 
-function addCreate(node) {
-	var createNode = {
-		innerHTML: "<img src=\"create.png\" onclick=create()>",
-		HTMLclass: "insert"
-	};
-	node.children.splice(node.children.length, 0, createNode);
-}
-
-function create() {
-	var node = chart_config.nodeStructure;
-	var newNode = {
-		text: { name: "work" + itemCounter++, priority: 1 }
-	};
-	node.children.splice(node.children.length - 1, 1, newNode);
+function rebuildTree() {
 	x.destroy();
 	leavesCount = 0;
-	init(node);
-	if (leavesCount < 6)
-		addCreate(node);
-	x = new Treant(chart_config);
+	hasInsertNode = false;
+	init(chartConfig.nodeStructure);
+	x = new Treant(chartConfig);
 }
 
-function byName(node, name) {
-	if (node.text.name == name)
+function findNodeByName(node, name) {
+	if (node.text.name == name) {
 		return node;
+	}
 	if (node.children) {
 		for(var i = 0; i < node.children.length; i++) {
-			childNode = byName(node.children[i], name);
-			if (childNode)
+			childNode = findNodeByName(node.children[i], name);
+			if (childNode) {
 				return childNode;
+			}
 		}
 	}
 	return undefined;
 }
 
-function changeText(input) {
-	var node = byName(chart_config.nodeStructure, input.name);
+function textChanged(input) {
+	var node = findNodeByName(chartConfig.nodeStructure, input.name);
 	input.name = input.value;
 	node.text.value = input.value;
 }
 
-function changePriority(input) {
-	var node = byName(chart_config.nodeStructure, input.name);
+function priorityChanged(input) {
+	var node = findNodeByName(chartConfig.nodeStructure, input.name);
 	node.text.priority = parseInt(input.value);
 }
 
-function removeNode(name) {
-	var node = byName(chart_config.nodeStructure, name);
+function createClick() {
+	var node = chartConfig.nodeStructure;
+	var newNode = {
+		text: { name: "work" + itemCounter++, priority: DEFAULT_PRIORITY }
+	};
+	node.children.splice(node.children.length - 1, 1, newNode);
+
+	rebuildTree();
+}
+
+function removeClick(name) {
+	var node = findNodeByName(chartConfig.nodeStructure, name);
 	var index = node.parent.children.indexOf(node);
 	if (node.children) {
 		node.children[0].parent = node.parent;
@@ -136,58 +144,68 @@ function removeNode(name) {
 		node.parent.children.splice(index, 1, node.children[0], node.children[1]);
 	}
 	else {
-		if (leavesCount == 6)
-			addCreate(node.parent);
 		node.parent.children.splice(index, 1);
 	}
-	x.destroy();
-	leavesCount = 0;
-	init(chart_config.nodeStructure);
-	x = new Treant(chart_config);
+
+	rebuildTree();
 }
 
-function union(name) {
-	var node1 = byName(chart_config.nodeStructure, name);
+function unionClick(name) {
+	var node1 = findNodeByName(chartConfig.nodeStructure, name);
 	var children = node1.parent.children;
 	var index = children.indexOf(node1);
 	var node2 = children[index + 1];
 
 	var node = {
-		text: { name: node1.text.name + node2.text.name, value: node1.text.value + node2.text.value },
+		text: {
+			name: node1.text.name + node2.text.name,
+			value: node1.text.value.substring(0, MAX_PAIR_LEN / 2) + node2.text.value.substring(0, MAX_PAIR_LEN / 2)
+		},
 		children: [node1, node2],
 	};
 	node1.parent.children.splice(index, 2, node);
-	x.destroy();
-	leavesCount = 0;
-	init(chart_config.nodeStructure);
-	x = new Treant(chart_config);
+
+	rebuildTree();
 }
 
-function up(name) {
-	return move(name, false);
-}
-
-function down(name) {
-	return move(name, true);
-}
-
-function move(name, down) {
-	var node = byName(chart_config.nodeStructure, name);
+function moveNode(name, down) {
+	var node = findNodeByName(chartConfig.nodeStructure, name);
 	var children = node.parent.children;
 	var index = children.indexOf(node);
 	var inc = down ? 1 : -1;
 	children[index] = children[index + inc];
 	children[index + inc] = node;
-	x.destroy();
-	leavesCount = 0;
-	init(chart_config.nodeStructure);
-	x = new Treant(chart_config);
+
+	rebuildTree();
+}
+
+function upClick(name) {
+	return moveNode(name, false);
+}
+
+function downClick(name) {
+	return moveNode(name, true);
 }
 
 var leavesCount = 0;
 var itemCounter = 0;
-init(chart_config.nodeStructure);
-if (leavesCount < 6) {
-	addCreate(chart_config.nodeStructure);
-}
+var hasInsertNode = false;
+var chartConfig = {
+	chart: {
+		container: "#taskTreeChart",
+		levelSeparation: 20,
+		rootOrientation: "WEST",
+		connectors: {
+			type: "curve",
+			style: { "stroke-width": 2, stroke: "#aaa" }
+		},
+		node: { HTMLclass: "cell" }
+	},
+	nodeStructure: {
+		text: { name: "" },
+		pseudo: "true",
+		children: JSON.parse(decodeURIComponent(location.search.split('tree=')[1]))
+	}
+};
 
+init(chartConfig.nodeStructure);
